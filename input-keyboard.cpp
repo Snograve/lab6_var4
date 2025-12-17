a8d58555bec51d1542b8dcff9e81a7742dc57d28
Yura <mr.gobrey@gmail.com>
2025-12-17 22:08:19 +0200

    Добавив свою частину коду

---
+++ input-keyboard.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    const int n = 14;
+
+    int B[n];
+
+    cout << "Enter 14 elements: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> B[i];
+    }
